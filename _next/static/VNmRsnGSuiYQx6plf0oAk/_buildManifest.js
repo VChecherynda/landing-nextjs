5ee653a7aca7c3089350bce3366632576262027d
VChecherynda <vadosevich@gmail.com>
2021-11-30 00:07:47 +0000

    Deploying to gh-pages from @ VChecherynda/landing-nextjs@10dc4c6fb304386da660bd9cff76ee4a9cf8cfd3 🚀

---
+++ _next/static/VNmRsnGSuiYQx6plf0oAk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/621-67ef0a9c1ea87f53.js","static/chunks/pages/index-55a2e48a10e1564d.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
