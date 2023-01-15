8fed33fd4e33cc350147748f20927e797371e9e7
vadym <checherinda.vadym@webbylab.com.com>
2023-01-15 19:39:25 +0200

    Delete config

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.PRODUCTION === 'true';
-
-module.exports = {
-    basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-    assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
-}
